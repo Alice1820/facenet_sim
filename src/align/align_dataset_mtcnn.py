45e2705e12b0ed2720b59fbbaf98d64bec7e320a
Alice <Alice1820@github.com>
2017-11-06 22:23:53 +0800

    align

--- src/align/align_dataset_mtcnn.py
+++ src/align/align_dataset_mtcnn.py
@@ -77,7 +77,9 @@ def main(args):
                 nrof_images_total += 1
                 filename = os.path.splitext(os.path.split(image_path)[1])[0]
                 output_filename = os.path.join(output_class_dir, filename+'.png')
+
                 print(image_path)
+                print (output_filename)
                 if not os.path.exists(output_filename):
                     try:
                         img = misc.imread(image_path)
